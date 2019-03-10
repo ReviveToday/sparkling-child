cf375739e20e622a3efeb398a8b7baf70fe6cf16
soup-bowl <code@revive.today>
2019-03-10 15:08:47 +0000

    Functions array syntax swap

--- customizer.php
+++ customizer.php
@@ -2,83 +2,83 @@
 
 add_action( 'customize_register', function( $wp_customize ) {
 	$wp_customize->add_setting(
-		'sparkling[site_layout]', array(
+		'sparkling[site_layout]', [
 			'default'           => 'full-width',
 			'type'              => 'option',
 			'sanitize_callback' => 'sparkling_sanitize_layout',
-		)
+		]
 	);
 
     $wp_customize->add_setting(
-		'sparkling[element_color]', array(
+		'sparkling[element_color]', [
 			'default'           => sanitize_hex_color( '#138E96' ),
 			'type'              => 'option',
 			'sanitize_callback' => 'sparkling_sanitize_hexcolor',
-		)
+		]
 	);
 
 	$wp_customize->add_setting(
-		'sparkling[link_color]', array(
+		'sparkling[link_color]', [
 			'default'           => sanitize_hex_color( '#020000' ),
 			'type'              => 'option',
 			'sanitize_callback' => 'sparkling_sanitize_hexcolor',
-		)
+		]
 	);
 
     $wp_customize->add_setting(
-		'sparkling[nav_bg_color]', array(
+		'sparkling[nav_bg_color]', [
 			'default'           => sanitize_hex_color( '#138E96' ),
 			'type'              => 'option',
 			'sanitize_callback' => 'sparkling_sanitize_hexcolor',
-		)
+		]
 	);
 
 	$wp_customize->add_setting(
-		'sparkling[nav_item_hover_color]', array(
+		'sparkling[nav_item_hover_color]', [
 			'default'           => sanitize_hex_color( '#CECECE' ),
 			'type'              => 'option',
 			'sanitize_callback' => 'sparkling_sanitize_hexcolor',
-		)
+		]
 	);
 
 	$wp_customize->add_setting(
-		'sparkling[nav_dropdown_bg]', array(
+		'sparkling[nav_dropdown_bg]', [
 			'default'           => sanitize_hex_color( '#020202' ),
 			'type'              => 'option',
 			'sanitize_callback' => 'sparkling_sanitize_hexcolor',
-		)
+		]
 	);
 
 	$wp_customize->add_setting(
-		'sparkling[nav_dropdown_item]', array(
+		'sparkling[nav_dropdown_item]', [
 			'default'           => sanitize_hex_color( '#D8CDCD' ),
 			'type'              => 'option',
 			'sanitize_callback' => 'sparkling_sanitize_hexcolor',
-		)
+		]
 	);
 
     $wp_customize->add_setting(
-		'sparkling[nav_dropdown_item_hover]', array(
+		'sparkling[nav_dropdown_item_hover]', [
 			'default'           => sanitize_hex_color( '#EDE1E1' ),
 			'type'              => 'option',
 			'sanitize_callback' => 'sparkling_sanitize_hexcolor',
-		)
+		]
 	);
 
 	$wp_customize->add_setting(
-		'sparkling[nav_dropdown_bg_hover]', array(
+		'sparkling[nav_dropdown_bg_hover]', [
 			'default'           => sanitize_hex_color( '#138E96' ),
 			'type'              => 'option',
 			'sanitize_callback' => 'sparkling_sanitize_hexcolor',
-		)
+		]
 	);
 
 	$wp_customize->add_setting(
-		'sparkling[custom_footer_text]', array(
+		'sparkling[custom_footer_text]', [
 			'default'           => 'Copyright © 2018 Revive Today. ',
 			'type'              => 'option',
 			'sanitize_callback' => 'sparkling_sanitize_strip_slashes',
-		)
+		]
 	);
 
 	$wp_customize->remove_section( 'epsilon_recomended_section' );
